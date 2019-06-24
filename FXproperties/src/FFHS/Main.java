23caf8694ad6e75edb7fa11b42784d46ba13c4b0
Theo <gmbaxevanos@hotmail.com>
2019-06-24 23:43:43 +0200

    properties

---
+++ FXproperties/src/FFHS/Main.java
@@ -0,0 +1,33 @@
+package FFHS;
+
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Main {
+
+    public static void main(final String[] args) {
+
+/*
+        //12.4.1
+
+        //access
+        final StringProperty textProp = new SimpleStringProperty("MICHA");
+
+        System.out.println("textProp: " + textProp);
+        System.out.println("getValue(): " + textProp.getValue() );
+
+        //bindings and lazy evaluation
+       // final BooleanBinding binding = new textProp.isEqualToIgnoreCase("micha");
+
+        //calculation
+        final IntegerProperty intProp1 = new SimpleIntegerProperty(10);
+        final IntegerProperty intProp2 = new SimpleIntegerProperty(12);
+
+        System.out.println("substract(): " + intProp1.add(40).subtract(intProp2));
+        System.out.println("multiply: " + intProp1.multiply(intProp2).getValue());*/
+
+    }
+}
