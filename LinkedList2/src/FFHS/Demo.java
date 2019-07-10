a9939e0ea2fc67b1bf25bce2f87556a4fdad0828
Theo <gmbaxevanos@hotmail.com>
2019-07-10 14:41:43 +0200

    ll2

---
+++ LinkedList2/src/FFHS/Demo.java
@@ -0,0 +1,70 @@
+package FFHS;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> placesTovisit = new LinkedList<>();
+
+/*        placesTovisit.add("Sydney");
+        placesTovisit.add("Salonica");
+        placesTovisit.add("Athens");
+        placesTovisit.add("Munchen");
+        placesTovisit.add("Bern");
+        placesTovisit.add("Zurich");
+        placesTovisit.add("Basel");*/
+
+        addInOrder(placesTovisit, "Thess");
+        addInOrder(placesTovisit, "Ath");
+        addInOrder(placesTovisit, "Basel");
+        addInOrder(placesTovisit, "Zurich");
+        
+        printList(placesTovisit);
+    }
+
+    private static void printList(LinkedList<String> linkedList) {
+        // we are going to use an Iterator -> go through the entries of a list
+
+        Iterator<String> i = linkedList.iterator();
+
+        while (i.hasNext()){
+            System.out.println("Now visiting " + i.next());
+        }
+        System.out.println("========================");
+    }
+
+    //sort the listed String items alphabetical (without duplicates)
+
+    private static boolean addInOrder(LinkedList<String> linkedList, String newCity){
+        ListIterator<String> stringListIterator =  linkedList.listIterator();
+
+        while (stringListIterator.hasNext()){
+            int comparison = stringListIterator.next().compareTo(newCity);
+            // this comparison gives us a number. If it is a zero (0), the values are equal
+
+            if (comparison == 0){
+                System.out.println(newCity + " is already included");
+                return false;
+            }
+            else if (comparison > 0){
+                // at that point, because we used the .next(), we are already at this element.
+                // but we want to go one element back, to add the new city at the right place
+
+                stringListIterator.previous(); // go back 1
+                stringListIterator.add(newCity); // so this way, it will be appear before this entry
+                return true;
+            }
+            else if (comparison < 0){
+                //move on to the next city
+                // the next() was already used, we are already there
+                // do not do anything
+            }
+        }
+        stringListIterator.add(newCity);
+        return true;
+    }
+}
---
+++ LinkedList2/src/FFHS/Main.java
@@ -0,0 +1,8 @@
+package FFHS;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
