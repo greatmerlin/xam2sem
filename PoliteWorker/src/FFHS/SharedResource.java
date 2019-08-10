0d95bd37d61c2769800bfdad0675ecfb4e7a846f
Theo <gmbaxevanos@hotmail.com>
2019-08-10 15:55:52 +0200

    pw

---
+++ PoliteWorker/src/FFHS/SharedResource.java
@@ -0,0 +1,20 @@
+package FFHS;
+
+public class SharedResource {
+
+    private Worker owner;
+
+    public SharedResource(Worker owner) {
+        this.owner = owner;
+    }
+
+    public Worker getOwner() {
+        return owner;
+    }
+
+    public synchronized void setOwner(Worker owner) {
+        this.owner = owner;
+    }
+
+
+}
