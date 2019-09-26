43b0791bf32744cbd06dc52175562ab1305704a4
Theo <gmbaxevanos@hotmail.com>
2019-09-26 15:15:55 +0200

    c1

---
+++ circularQx1/src/FFHS/Main.java
@@ -0,0 +1,59 @@
+package FFHS;
+
+/*
+* we wrap the back of the q to the start of the array, otherwise although we don't reach the 5 elements (fix array size
+* at this example), the array ist getting doubled (see code, back is incremented to 5).
+*
+*
+* */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+        Employee billEnd = new Employee("Bill", "End", 78);
+
+        ArrayQueue queue = new ArrayQueue(5);
+
+        queue.add(janeJones);
+        queue.add(johnDoe);
+        queue.remove();
+        queue.add(marySmith);
+        queue.remove();
+        queue.add(mikeWilson);
+        queue.remove();
+        queue.add(billEnd);
+        queue.remove();
+        queue.add(janeJones);
+
+        queue.printQueue();
+
+//        queue.add(janeJones);
+//        queue.add(johnDoe);
+//        queue.add(marySmith);
+//        queue.add(mikeWilson);
+//        queue.add(billEnd);
+//        //queue.printQueue();
+//
+//        queue.remove();
+//        queue.remove();
+//        //queue.printQueue();
+//
+//        //System.out.println(queue.peek());
+//        queue.remove();
+//        queue.remove();
+//        queue.remove();
+//        //queue.remove();
+////
+//        queue.add(mikeWilson);
+////
+////
+//        queue.printQueue();
+
+
+    }
+}
