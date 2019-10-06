b6d97d795c3a79fbac967aca04957c6ee7fc94cd
Theo <gmbaxevanos@hotmail.com>
2019-10-07 00:17:39 +0200

    llex11

---
+++ linkedListEx/src/FFHS/Main.java
@@ -0,0 +1,60 @@
+package FFHS;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+        Employee billEnd = new Employee("Bill", "End", 78);
+
+        LinkedList<Employee> list = new LinkedList<>();
+        list.addFirst(janeJones);
+        list.addFirst(johnDoe);
+        list.addFirst(marySmith);
+        list.addFirst(mikeWilson);
+
+        Iterator iter = list.iterator();
+        System.out.print("HEAD -> ");       // at the start of the list it will print the head
+        while (iter.hasNext()) {
+            System.out.print(iter.next());  // print out the list items
+            System.out.print("<=>");
+        }
+        System.out.println("null");         // at the end of the list it will print the null
+
+        list.addLast(billEnd);
+        iter = list.iterator();
+        System.out.print("HEAD -> ");
+        while (iter.hasNext()) {
+            System.out.print(iter.next());
+            System.out.print("<=>");
+        }
+        System.out.println("null");
+
+        list.removeFirst();
+        iter = list.iterator();
+        System.out.print("HEAD -> ");
+        while (iter.hasNext()) {
+            System.out.print(iter.next());
+            System.out.print("<=>");
+        }
+        System.out.println("null");
+
+        list.removeLast();
+        iter = list.iterator();
+        System.out.print("HEAD -> ");
+        while (iter.hasNext()) {
+            System.out.print(iter.next());
+            System.out.print("<=>");
+        }
+        System.out.println("null");
+
+//        for (Employee employee: list) {
+//            System.out.println(employee);
+//        }
+    }
+}
