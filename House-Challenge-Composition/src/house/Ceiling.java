a6219a7f73b4f7c507474c99d0335054e8221959
greatmerlin <gmbaxevanos@gmx.ch>
2019-11-09 19:38:08 +0100

    hc

---
+++ House-Challenge-Composition/src/house/Ceiling.java
@@ -0,0 +1,20 @@
+package house;
+
+public class Ceiling {
+
+    private int height;
+    private String colour;
+
+    public Ceiling(int height, String colour) {
+        this.height = height;
+        this.colour = colour;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+}
